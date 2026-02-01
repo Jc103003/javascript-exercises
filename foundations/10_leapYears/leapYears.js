e56e4664265a7c3e75e9ff8574af080bfc086de1
James Caldwell <james.c103003@gmail.com>
2026-01-31 23:41:23 -0500

    compelted 07 - 09

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,34 @@
-const leapYears = function() {
-
+const leapYears1 = function(year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true
+            }
+            else {
+                return false
+            }
+        }
+        else {
+            return true
+        }
+    }
+    else {
+        return false
+    }
 };
+
+const leapYears = function(year) {
+    const isDivisbleBy4 = year % 4 == 0
+    const isDivisbleBy100 = year % 100 == 0
+    const isDivisibleBy400 = year % 400 == 0
+
+    if (isDivisbleBy4 && !isDivisbleBy100 || isDivisibleBy400){
+        return true
+    }
+    else {
+        return false
+    }
+}
 
 // Do not edit below this line
 module.exports = leapYears;
